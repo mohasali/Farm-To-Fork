8ea1dd0f2cff287211af449e67ebbdf1de6ccf92
mohasali <msalim1244@gmail.com>
2024-10-31 12:26:29 +0000

    Updated boxes index and show
    Updated the views to be compatible with mobile devices

--- resources/views/boxes/show.blade.php
+++ resources/views/boxes/show.blade.php
@@ -1,11 +1,13 @@
 <x-layout>
-<div class="flex flex-col justify-content-start mt-8 h-[calc(100vh-200px)]">
-    <div class=" flex flex-row mx-auto space-x-6 p-8">
-        <div class=" pt-7 "> <img class=" max-w-[450px] rounded-lg" src = "{{ $box->imagePath }}" alt="Box"> </div>
+<div class="flex flex-col justify-content-start mt-8 min-h-[calc(100vh-200px)]">
+    <div class=" flex flex-col md:flex-row mx-auto space-x-6 p-8">
+        <a class="md:hidden text-3xl mb-2 font-light italic underline hover:text-accent2" href="/boxes?type={{ $box->type }}">{{ $box->type }}</a>
+        <div class="md:hidden text-4xl mb-3 font-medium"> {{ $box->title }}</div>
+        <div class="flex justify-center md:block pt-7 "> <img class="w-[100%] md:w-[450px] rounded-lg" src = "{{ $box->imagePath }}" alt="Box"> </div>
         <div> 
-            <a class=" text-xl mb-2 font-light italic hover:underline hover:text-accent2" href="/boxes?type={{ $box->type }}">{{ $box->type }}</a>
-            <div class=" text-3xl mb-3 font-medium"> {{ $box->title }}</div>
-            <div class=" text-lg space-x-2 mb-2 font-light italic">
+            <a class=" hidden md:inline-block text-xl mb-2 font-light italic hover:underline hover:text-accent2" href="/boxes?type={{ $box->type }}">{{ $box->type }}</a>
+            <div class=" hidden md:block text-3xl mb-3 font-medium"> {{ $box->title }}</div>
+            <div class=" text-xl md:text-lg mt-3 md:mt-0 space-x-2 mb-2 font-light underline md:no-underline italic">
                 <a class="hover:underline hover:text-accent2" href="#">Summer</a>
                 <a class="hover:underline hover:text-accent2" href="#">Greens</a>
                 <a class="hover:underline hover:text-accent2" href="#">Low Fat</a>
