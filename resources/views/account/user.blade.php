8a73b687b1ef21a19aaa2988cf1026598c6f434c
mohasali <msalim1244@gmail.com>
2024-11-07 14:15:00 +0000

    Account pages
    User can press edit to edit his account details

--- resources/views/account/user.blade.php
+++ resources/views/account/user.blade.php
@@ -3,13 +3,11 @@
     <p class="text-sm text-gray-500 mb-4"></p>
 
     <!-- Personal Information Form -->
-    <form class="grid grid-cols-1 sm:grid-cols-2 gap-4" method="POST" action='/user'>
-        @csrf
-        @method('PATCH')
-        <!-- Full Name -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <!-- Username -->
         <div class="col-span-2">
             <label class="block text-gray-700 text-sm font-semibold mb-1">Name</label>
-            <input type="text" class="w-full p-2 border border-gray-300 rounded" id="name" name="name" value="{{ Auth::user()->name }}" required>
+            <input type="text" class="w-full p-2 border border-gray-300 rounded" value="{{ Auth::user()->name }}" disabled>
         </div>
 
         <!-- Email -->
@@ -21,38 +19,32 @@
 
             $var = (count($parts) > 1) ? '@' . strtolower($parts[1]) : '@';
             @endphp
-            <input type="email" name="email" id="email" class="w-full p-2 border border-gray-300 rounded" value="{{ Auth::user()->email}}" required>
+            <input type="email" class="w-full p-2 border border-gray-300 rounded" value="{{ substr(Auth::user()->email, 0, 1) }}********{{ substr(explode('@', Auth::user()->email)[0], -1) }}{{ $var }}" disabled>
         </div>
-        <x-form-error name="email"/>
 
         <!-- Phone -->
         <div class="col-span-1">
+            @php
+            $phone = Auth::user()->phone;
+            if($phone!=''){ $phone = "********".substr(Auth::user()->phone, -3);};
+            @endphp
             <label class="block text-gray-700 text-sm font-semibold mb-1">Phone</label>
-            <input type="text" id="phone" name="phone" class="w-full p-2 border border-gray-300 rounded " value="{{ Auth::user()->phone }}" >
+            <input type="text" class="w-full p-2 border border-gray-300 rounded" value="{{$phone}}" disabled> <!-- Add Phone number -->
         </div>
-        <x-form-error name="phone"/>
 
         <!-- Password -->
         <div class="col-span-1">
             <label class="block text-gray-700 text-sm font-semibold mb-1">Password</label>
-            <input type="password" name="password" id="password" class="w-full p-2 border border-gray-300 rounded">
+            <input type="password" class="w-full p-2 border border-gray-300 rounded" value="********" disabled>
         </div>
-        <div class="col-span-1">
-            <label class="block text-gray-700 text-sm font-semibold mb-1">Confirm Password</label>
-            <input type="password" name="password_confirmation" id="password_confirmation" class="w-full p-2 border border-gray-300 rounded">
+        <!-- Buttons -->
+        <div class="col-span-2 flex space-x-4 mt-6">
+            <a href="{{ route('account.edit') }}" type="submit" class="bg-primary text-white px-4 py-2 rounded font-semibold hover:bg-orange-600">Edit</a>
         </div>
-        <x-form-error name="password"/>
-
         @if (session('success'))
         <div class=" text-green-600 font-bold text-sm mt-1">
             {{ session('success') }}
         </div>
         @endif
-
-        <!-- Buttons -->
-        <div class="col-span-2 flex space-x-4 mt-6">
-            <button type="submit" class="bg-primary text-white px-4 py-2 rounded font-semibold hover:bg-orange-600">Save</button>
-            <a href='/account' class="bg-gray-300 text-gray-700 px-4 py-2 rounded font-semibold hover:bg-gray-400">Cancel</a>
-        </div>
-    </form>
+    </div>
 </x-account-layout>
