0f15db3239c85539755e9088dcb5af09cd1db9b6
Hamza <114071457+Hamza2702@users.noreply.github.com>
2024-11-27 12:45:56 +0000

    Comments and address page
    Added comments for route
    Created somewhat of a layout for the address page

--- resources/views/account/address.blade.php
+++ resources/views/account/address.blade.php
@@ -1,4 +1,14 @@
 <x-account-layout>
     <h1 class="text-2xl font-semibold mb-2">My Address</h1>
     <p class="text-sm text-gray-500 mb-4"></p>
+    <div class="flex items-stretch space-x-4">
+        <!-- Create address -->
+        <div class="flex bg-gray-100 py-4 rounded-xl justify-center items-center w-[35%] h-[275px]">
+            <p class="m-auto text-2xl" style="opacity: 0.75;">Add Address</p>
+        </div>
+        <!-- Current address -->
+        <div class="flex bg-gray-100 rounded-xl justify-center items-center w-[35%] h-[275px]">
+            <p class="m-auto">Place Holder Text</p>
+        </div>
+    </div>
 </x-account-layout>
