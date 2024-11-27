514d57c527c4eaa2716db2ee042f34bc26c8a881
Mango-Goose <113191075+Mango-Goose@users.noreply.github.com>
2024-11-27 12:03:32 +0000

    Updated User Register to Include Address Input
    Only done in front end - working on adding the relevant tables and backend with this.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
         <h3 class=" font-bold text-5xl text-center pb-9 ">Register</h3>
         @csrf
 
+        <!-- Registration form inputs-->
         <x-form-input name="name" id="name" type="text" :value="old('name')" label="Full Name"/>
         <x-form-error name="name"/>
 
@@ -13,9 +14,26 @@
         <x-form-input  name="email_confirmation" id="email_confirmation" type="email" :value="old('email_confirmation')" label="Confirm Email"/>
         <x-form-error name="email_confirmation"/>
 
-        <x-form-input  name="phone" id="phone" type="phone" :value="old('phone')" label="Phone" />
+        <x-form-input  name="phone" id="phone" type="tel" :value="old('phone')" label="Phone" />
         <x-form-error name="phone"/>
 
+        <!-- Address Details !!Make Optional -->
+        <x-form-input  name="address_line_1" id="address_line_1" type="phone" :value="old('address_line_1')" label="Address Line 1" />
+        <x-form-error name="address_line_1"/>
+
+        <x-form-input  name="address_line_2" id="address_line_2" type="phone" :value="old('address_line_2')" label="Address Line 2" />
+        <x-form-error name="address_line_2"/>
+
+        <x-form-input  name="postcode" id="postcode" type="phone" :value="old('postcode')" label="Postcode" />
+        <x-form-error name="postcode"/>
+
+        <x-form-input  name="town/city" id="town/city" type="phone" :value="old('town/city')" label="Town/ City" />
+        <x-form-error name="town/city"/>
+
+        <x-form-input  name="country" id="country" type="phone" :value="old('country')" label="Country" />
+        <x-form-error name="country"/>
+
+        <!-- Password Details -->
         <x-form-input name="password" id="password" type="password" label="Password"/>
         <x-form-error name="password"/>
 
