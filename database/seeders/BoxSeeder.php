2e84d9a1feac8ca45b933529b7ab74da6ea97851
PeaceRotimi <147106552+PeaceRotimi@users.noreply.github.com>
2024-11-29 13:54:36 +0000

    Updated the Box Seeder

--- database/seeders/BoxSeeder.php
+++ database/seeders/BoxSeeder.php
@@ -4,14 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Box
 
 class BoxSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+    Box::create({
+        'title' => ,
+            'type' => ,
+            'description' => ,
+            'price' => ,
+            'imagePath' => '/images/crate.jpg'
+    })
     }
 }
