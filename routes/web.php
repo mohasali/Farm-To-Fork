792613b8e1d5a10d87e4260852fa2b838149fa22
Hamza <HamzaKhan2702@outlook.com>
2024-12-03 22:55:51 +0000

    Recipes Page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\SessionController;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 // Home / Index
@@ -68,3 +69,8 @@ Route::get('/order/confirmed',[OrderController::class,'confirmed'])->middleware(
 
 //connecting address database table to the register input form
 Route::post('/user_addresses', [UserAddressesController::class, 'store'])->name('user.addresses');
+
+// Recipes
+Route::middleware('auth')->controller(RecipeController::class)->group(function() {
+    Route::get('/recipes','recipes')->name('recipes.recipes');
+});
