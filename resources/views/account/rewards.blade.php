965a4b10e51c7a92d763ee1f40dba1f7130af108
Hamza <114071457+Hamza2702@users.noreply.github.com>
2024-12-20 15:47:11 +0000

    Updated rewards page

--- resources/views/account/rewards.blade.php
+++ resources/views/account/rewards.blade.php
@@ -1,4 +1,18 @@
 <x-account-layout>
     <h1 class="text-2xl font-semibold mb-2">Your Rewards</h1>
-    <p class="text-sm text-gray-500 mb-4"></p>
+    <p class="text-sm text-gray-500 mb-4">Current Points: 000000</p>
+    <!-- Stamp system -->
+    <div>
+        <h1 class="text-1xl font-semibold mb-2">Daily Rewards</h1>
+        <div class="flex justify-center">
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4 p-6">
+                <button class="rounded-full border-solid border-black bg-gray-100 w-20 h-20 md:w-24 md:h-24 flex items-center justify-center hover:bg-gray-200"></button>
+                <button class="rounded-full bg-gray-100 w-20 h-20 md:w-24 md:h-24 flex items-center justify-center hover:bg-gray-200"></button>
+                <button class="rounded-full bg-gray-100 w-20 h-20 md:w-24 md:h-24 flex items-center justify-center hover:bg-gray-200"></button>
+                <button class="rounded-full bg-gray-100 w-20 h-20 md:w-24 md:h-24 flex items-center justify-center hover:bg-gray-200"></button>
+                <button class="rounded-full bg-gray-100 w-20 h-20 md:w-24 md:h-24 flex items-center justify-center hover:bg-gray-200"></button>
+                <button class="rounded-full bg-gray-100 w-20 h-20 md:w-24 md:h-24 flex items-center justify-center hover:bg-gray-200"></button>
+            </div>
+        </div>
+    </div>
 </x-account-layout>
