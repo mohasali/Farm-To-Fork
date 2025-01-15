a5c3116c85dbefcd0023f22eb5d645e6037a2301
Hamza <HamzaKhan2702@outlook.com>
2025-01-15 00:05:07 +0000

    Comment reviews frontend
    - Removed reviews from recipes and added to boxes instead
    - Added a moon (star) system
    - Filter by top reviews and most recent
    - Reviews include their name, rating, title, where and when it was reviewed, if it was a verified purchase and their comment
    - Reviews have a helpful button (int counter?) and a report button

--- resources/views/boxes/show.blade.php
+++ resources/views/boxes/show.blade.php
@@ -39,6 +39,124 @@
                 <x-success-alert :boxId="$box->id" />
             </div>
         </div>
+        
+        <!-- Comment section -->
+        <hr class="hrline"/>
+        <div class="flex justify-start items-start h-full pl-96 ">
+            <div class="flex flex-col bg-gray-100 py-2 rounded-xl mt-4 w-1/3 sm:w-[33%] md:h-[375px]">      
+                <h1 class="text-xl pl-4 font-bold">Customer Reviews</h1>
+                <p class="text-l pl-4">🌕🌕🌕🌕🌗</p> <!-- moon not stars :P -->
+                <p class="text-l pl-4">4.5 out of 5</p> <!-- Make real ratings later -->
+                <p class="pl-4">100 global ratings<p>
+                <!-- If there is no rating on a star, there isn't a link for it | MUST ADD TO 100% -->
+                <div class="flex items-center space-x-4 pl-4 mt-4">
+                    <!-- Rating -->
+                    <p>5 star</p>
+                    <!-- Add a percent bar later -->
+                    <div class="w-[75%] h-8 bg-gray-300 rounded-md"></div>
+                    <!-- Percent -->
+                    <p>60%</p>
+                </div>
+                <div class="flex items-center space-x-4 pl-4 mt-4">
+                    <!-- Rating -->
+                    <p>4 star</p>
+                    <!-- Add a percent bar later -->
+                    <div class="w-[75%] h-8 bg-gray-300 rounded-md"></div>
+                    <!-- Percent -->
+                    <p>20%</p>
+                </div>
+                <div class="flex items-center space-x-4 pl-4 mt-4">
+                    <!-- Rating -->
+                    <p>3 star</p>
+                    <!-- Add a percent bar later -->
+                    <div class="w-[75%] h-8 bg-gray-300 rounded-md"></div>
+                    <!-- Percent -->
+                    <p>0%</p>
+                </div>
+                <div class="flex items-center space-x-4 pl-4 mt-4">
+                    <!-- Rating -->
+                    <p>2 star</p>
+                    <!-- Add a percent bar later -->
+                    <div class="w-[75%] h-8 bg-gray-300 rounded-md"></div>
+                    <!-- Percent -->
+                    <p>10%</p>
+                </div>
+                <div class="flex items-center space-x-4 pl-4 mt-4">
+                    <!-- Rating -->
+                    <p>1 star</p>
+                    <!-- Add a percent bar later -->
+                    <div class="w-[75%] h-8 bg-gray-300 rounded-md"></div>
+                    <!-- Percent -->
+                    <p>10%</p>
+                </div>
+            </div>
+            <div class="flex flex-col bg-gray-100 py-2 mb-2 rounded-xl ml-4 mt-4 w-1/3 sm:w-[41%] md:h-[675px]">      
+                <!-- Filter reviews -->
+                <div class="pl-4 rounded-md">
+                    <select name="review" id="review" class="rounded-md bg-gray-200 p-0.5 text-sm">
+                        <option value="top">Top reviews</option>
+                        <option value="recent">Most recent</option>
+                    </select>
+                </div>
+                <!-- Reviews -->
+                <!-- Implement a for each loop after -->
+                <div class="p-4">
+                    <!-- 
+                    Potential icon? | Name 
+                    Rating | Title
+                    Reviewed in Birmingham on 2 January 2025
+                    Verified Purchase
+                    Comment
+                    Helpful | Report
+                    -->
+                    <h1 class="text-lg font-bold">Jane Doe</h1>
+                    <div class="flex items-center">
+                        <p>🌕🌕🌕🌕🌑</p>
+                        <p class="pl-2 font-bold">I love this box!</p>
+                    </div>
+                    <p class="text-xs">Reviewed in Manchester on 15 January 2025</p>
+                    <p class="font-bold text-primary text-xs">Verified Purchase</p>
+                    <!-- Comment -->
+                    <div>
+                        <p class="text-sm" style="word-wrap: break-word;">
+                            This offering was a delightful surprise! The flavors were vibrant, and the ingredients were exceptionally fresh. 
+                            A true treat for anyone seeking a delicious and healthy culinary adventure.
+                        </p>
+                    </div>
+                    <div class="flex items-center mt-2">
+                        <button class="border border-primary text-xs text-primary rounded-full px-4 py-1 mr-2">
+                            Helpful
+                        </button>
+                        <p class="pr-2">|</p>
+                        <p class="text-xs">Report</p> <!-- Add report feature ig or keep it blank or scrap it -->
+                    </div>
+                </div>
+                <div class="p-4 mt-2">
+                    <h1 class="text-lg font-bold">John Doe</h1>
+                    <div class="flex items-center">
+                        <p>🌕🌕🌕🌕🌑</p>
+                        <p class="pl-2 font-bold">Tasty!</p>
+                    </div>
+                    <p class="text-xs">Reviewed in Birmingham on 14 January 2025</p>
+                    <p class="font-bold text-primary text-xs">Verified Purchase</p>
+                    <!-- Comment -->
+                    <div>
+                        <p class="text-sm" style="word-wrap: break-word;">
+                            Listening to Radiohead and starving on a Tuesday night, I purchased this box thinking of all the delicious ingredients
+                            and flavours that will enter my mouth when it arrives. AND IT DIDN'T DISAPPOINT! I absolutely love Farm to Fork!
+                        </p>
+                    </div>
+                    <div class="flex items-center mt-2">
+                        <button class="border border-primary text-xs text-primary rounded-full px-4 py-1 mr-2">
+                            Helpful
+                        </button>
+                        <p class="pr-2">|</p>
+                        <p class="text-xs">Report</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
     <!-- import the javascript -->
     @vite(['resources/js/glass.js'])
